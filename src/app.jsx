989ce3bb5290ba926e35f943ef3d1bd9a079912e
Benjamin Chacko <benjaminchacko@outlook.com>
2020-07-03 17:01:14 -0600

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react'
+
+const name = "Ben Chacko";
+
+const App = () => {
+    return (
+      <Fragment>
+        <h1>Hello {name}!</h1>
+      </Fragment>
+    );
+  };
+
+export default App
